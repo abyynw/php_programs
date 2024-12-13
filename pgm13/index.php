1b7611c2c723318ad4cb84e826a259ed4ee607de
abyynw <abhinavachu672@gmail.com>
2024-12-13 11:01:53 +0530

    first commit

--- pgm13/index.php
+++ pgm13/index.php
@@ -7,11 +7,30 @@
 </head>
 <body>
     <center>
-        <form action="">
+        <form action="index.php" method="post">
             <label for="">Enter the units</label>
-            <input type="text">
+            <input type="text" id="units" name="units" required><br><br>
+            <label for="">Total Amount is : </label>
+            <input type="text" id="amount" name="amount" readonly><br><br>
             <button>Submit</button>
         </form>
     </center>
+    <?php
+    if (isset($_POST["units"])){
+        $units = $_POST["units"];
+        if($units > 200){
+            $rate = 5;
+        }
+        elseif($units >100){
+            $rate = 4;
+        }
+        else{
+            $rate = 3;
+        }
+        $amount = $rate * $units;
+        echo "<script>document.getElementById('amount').value=$amount</script>";
+    }
+    
+    ?>
 </body>
 </html>
